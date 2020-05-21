6e971767cc8437b7574609cc6640b3e92019789e
milinbhakta@gmail.com <desire2dosomething@gmail.com>
2020-05-21 12:03:13 -0400

    * .gitignore: initial commit
    * Xamarinapptest1.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * Info.plist:
    * Item.cs:
    * AssemblyInfo.cs:
    * AppDelegate.cs:
    * MainPage.xaml:
    * ItemsPage.xaml:
    * AboutPage.xaml:
    * NewItemPage.xaml:
    * MainPage.xaml.cs:
    * IDataStore.cs:
    * Entitlements.plist:
    * Xamarinapptest1.csproj:
    * AboutPage.xaml.cs:
    * MainActivity.cs:
    * ItemsPage.xaml.cs:
    * Default.png:
    * MockDataStore.cs:
    * NewItemPage.xaml.cs:
    * ItemDetailPage.xaml:
    * tab_feed.png:
    * BaseViewModel.cs:
    * tab_about.png:
    * ItemsViewModel.cs:
    * ItemDetailPage.xaml.cs:
    * AboutViewModel.cs:
    * Default@2x.png:
    * tab_feed@2x.png:
    * tab_feed@3x.png:
    * tab_about@3x.png:
    * AssemblyInfo.cs:
    * tab_about@2x.png:
    * xamarin_logo.png:
    * AboutAssets.txt:
    * Xamarinapptest1.iOS.csproj:
    * xamarin_logo@3x.png:
    * ItemDetailViewModel.cs:
    * Default-568h@2x.png:
    * xamarin_logo@2x.png:
    * Default-Portrait.png:
    * AssemblyInfo.cs:
    * Tabbar.xml:
    * styles.xml:
    * colors.xml:
    * AboutResources.txt:
    * Toolbar.xml:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * icon.png:
    * AndroidManifest.xml:
    * Resource.designer.cs:
    * Xamarinapptest1.Android.csproj:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * xamarin_logo.png:
    * icon.xml:
    * Icon40.png:
    * Icon60.png:
    * Icon87.png:
    * Icon29.png:
    * Icon20.png:
    * Icon80.png:
    * Icon76.png:
    * Icon58.png:
    * Icon180.png:
    * Icon152.png:
    * Icon167.png:
    * icon_round.xml:
    * Icon120.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ Xamarinapptest1/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarinapptest1.Services;
+using Xamarinapptest1.Views;
+
+namespace Xamarinapptest1
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
